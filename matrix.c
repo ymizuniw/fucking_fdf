971946b5a1f5e037cc24ff81010a35d8857deff2
Yugo Mizuniwa <ymizuniw@c2r8s7.42tokyo.jp>
2025-08-02 06:04:10 +0900

    readme update, rotation and proj_iso implementation is proceeding.

--- matrix.c
+++ matrix.c
@@ -1,16 +1,9 @@
 #include "includes/fdf.h"
 
-typedef struct s_point
-{
-    float x;
-    float y;
-    float z;
-} t_point;
-
-void rotate_x(t_point *p, float angle);
-void rotate_y(t_point *p, float angle);
-void rotate_z(t_point *p, float angle);
-void project_iso(t_point *p);
+void		rotate_x(t_vec_3 *p, float t);
+void		rotate_y(t_vec_3 *p, float t);
+void		rotate_z(t_vec_3 *p, float t);
+void		project_iso(t_vec_3 *p);
 
 // keep x
 // {
@@ -31,20 +24,61 @@ void project_iso(t_point *p);
 //     0           0           1
 // }
 
-void    rotate_x(t_point *p, float angle)
+typedef struct s_vec_3
 {
-    tmp_y = p->y * cos(t) - p->z * sin(t);
-    tmp_z = p->y * sin(t) + p->z * cos(t);
+	float	x;
+	float	y;
+	float	z;
+}			t_vec_3;
+
+typedef struct s_vec_2
+{
+	float	x;
+	float	y;
+}			t_vec_2;
+
+typedef struct s_matrix
+{
+	int		theta_x;
+	int		theta_y;
+	int		theta_z;
+	float	scale;
+}			t_matrix;
+
+transform_object(t_vec_3 p, t_matrix *mat)
+{
+    t_vec_2 new;
+	
+    rotate_x(&p, mat->theta_x);
+	rotate_y(&p, mat->theta_y);
+	rotate_z(&p, mat->theta_z);
+	new = proj_iso(p);
 }
 
-void    rotate_y(t_point *p, float angle)
+void	rotate_x(t_vec_3 *p, float t)
 {
-    tmp_x = p->x * cos(t) + p->z * sin(t);
-    tmp_z = p->x * (-sin(t)) + p->z * cos(t);
+	p->y = p->y * cos(t) - p->z * sin(t);
+	p->z = p->y * sin(t) + p->z * cos(t);
 }
 
-void    rotate_z(t_point *p, float angle)
+void	rotate_y(t_vec_3 *p, float t)
 {
-    tmp_x = p->x * cos(t) - p->y * sin(t);
-    tmp_y = p->x * sin(t) + p->y * cos(t);
+	p->x = p->x * cos(t) + p->z * sin(t);
+	p->z = p->x * (-sin(t)) + p->z * cos(t);
+}
+
+void	rotate_z(t_vec_3 *p, float t)
+{
+	p->x = p->x * cos(t) - p->y * sin(t);
+	p->y = p->x * sin(t) + p->y * cos(t);
+}
+
+//isometric projection using angle t = 30;
+t_vec_2	proj_iso(t_vec_3 p)
+{
+	t_vec_2 res;
+
+	res.x = (p.x - p.y) * cos(30);
+	res.y = (p.x + p.y) * sin(30) - p.z;
+	return (res);
 }
